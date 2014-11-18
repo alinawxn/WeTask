7d06fae4b3b3c38be8cd6e0f02ef16e28e760528
XN <XN@XNs-MacBook-Air.local>
2014-11-18 11:22:14 -0500

    Initial Commit

---
+++ WeTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeTask
+//
+//  Created by XN on 11/18/14.
+//  Copyright (c) 2014 Concordia InforTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeTask/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  WeTask
+//
+//  Created by XN on 11/18/14.
+//  Copyright (c) 2014 Concordia InforTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
